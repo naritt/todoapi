be248b46d1956ff094643e2f59fe96c7938ebb8c
Naritt <40392657+naritt@users.noreply.github.com>
2021-09-28 11:02:44 +0700

    Update TodoItemsController.cs

--- testapi/Controllers/TodoItemsController.cs
+++ testapi/Controllers/TodoItemsController.cs
@@ -42,7 +42,6 @@ namespace testapi.Controllers
         }
 
         // PUT: api/TodoItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
         {
